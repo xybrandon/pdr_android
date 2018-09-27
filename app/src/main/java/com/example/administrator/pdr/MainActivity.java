843f560342d94a9f42436ceeb776c5cfb9be263a
yuekang <xy526575008@163.com>
2018-09-27 16:14:39 +0800

    m

--- app/src/main/java/com/example/administrator/pdr/MainActivity.java
+++ app/src/main/java/com/example/administrator/pdr/MainActivity.java
@@ -140,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 Message msg = new Message();
-                msg.what = 15;
+                msg.what = 2;
 				msg.obj = pdrData;
                 mHandler.sendMessage(msg);
         }
